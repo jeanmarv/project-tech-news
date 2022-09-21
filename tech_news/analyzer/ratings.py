faaa0cd6d461153d351a3194bea3fd4249be7f0f
Jean Marcel <jeanvanzella91@gmail.com>
2022-09-21 19:43:33 -0300

    req 10-11, return top5news and top 5 categories

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,8 +1,36 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    top_five = []
+    results = find_news()
+    sorted_data = sorted(
+        results, key=lambda data: data["comments_count"], reverse=True
+    )[:5]
+
+    for itens in sorted_data:
+        top_five.append((itens["title"], itens["url"]))
+
+    return top_five
 
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    get_news = find_news()
+    sum_categories = {}
+    category_result = []
+
+    for itens in get_news:
+        if itens['category'] in sum_categories:
+            sum_categories[itens['category']] += 1
+        else:
+            sum_categories[itens["category"]] = 0
+
+    sort_categories = sorted(
+        sum_categories.items(), key=lambda x: (-x[1], x[0]))
+
+    for itens in sort_categories:
+        category_result.append(itens[0])
+
+    return category_result
