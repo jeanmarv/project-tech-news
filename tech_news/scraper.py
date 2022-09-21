381bb3fab6d23e133e46689dcb551a7d071a4f91
Jean Marcel <jeanvanzella91@gmail.com>
2022-09-21 16:40:14 -0300

    building a full scrape of news page

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -34,7 +34,27 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = parsel.Selector(text=html_content)
+    url = selector.css("link[rel=canonical]::attr(href)").get()
+    title = selector.css(".entry-title::text").get().strip()
+    timestamp = selector.css("li.meta-date::text").get()
+    writer = selector.css(".author a::text").get()
+    comments_count = len(selector.css("div.comment-body").getall())
+    summary = "".join(
+        selector.css(".entry-content > p:first-of-type *::text").getall()
+    ).strip()
+    tags = selector.css("section.post-tags ul li a::text").getall()
+    category = selector.css("div.meta-category span.label::text").get()
+    return {
+    "url": url,
+    "title": title,
+    "timestamp": timestamp,
+    "writer": writer,
+    "summary": summary,
+    "comments_count": comments_count,
+    "tags": tags,
+    "category": category
+    }
 
 
 # Requisito 5
