723956f48eefbb4f1b948f8914d4c893d5e2d750
Jean Marcel <jeanvanzella91@gmail.com>
2022-09-21 18:54:48 -0300

    search by title and by date, req 6-7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,11 +1,30 @@
+from tech_news.database import search_news
+from datetime import datetime
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    titles = []
+    title_url = search_news({"title": {"$regex": title, "$options": "i"}})
+
+    for itens in title_url:
+        titles.append((itens["title"], itens["url"]))
+
+    return titles
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    news_by_date = []
+    try:
+        date_str = datetime.strptime(date, "%Y-%m-%d").strftime("%d/%m/%Y")
+        results = search_news({"timestamp": date_str})
+    except ValueError:
+        raise ValueError("Data inválida")
+    else:
+        for itens in results:
+            news_by_date.append((itens["title"], itens["url"]))
+        return news_by_date
 
 
 # Requisito 8
